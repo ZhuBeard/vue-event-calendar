0979b0a22e717d9cf97c92c85fea6894bee04d04
zjc <276019280@qq.com>
2019-01-08 18:37:25 +0800

    tj

--- src/components/cal-events.vue
+++ src/components/cal-events.vue
@@ -57,14 +57,14 @@ export default {
     dayEventsTitle () {
       if (this.title) return this.title
       // if (this.dayEvents.date !== 'all') {
-      //   let tempDate
-      //   if (this.dayEvents.events.length !== 0) {
-      //     tempDate = Date.parse(new Date(this.dayEvents.events[0].date))
-      //     return dateTimeFormatter(tempDate, i18n[this.locale].fullFormat)
-      //   } else {
-      //     tempDate = dateTimeFormatter(Date.parse(new Date(this.dayEvents.date)), i18n[this.locale].fullFormat)
-      //     return `${tempDate} ${i18n[this.locale].notHaveEvents}`
-      //   }
+        let tempDate
+        if (this.dayEvents.events.length !== 0) {
+          tempDate = Date.parse(new Date(this.dayEvents.events[0].date))
+          return dateTimeFormatter(tempDate, i18n[this.locale].fullFormat)
+        } else {
+          tempDate = dateTimeFormatter(Date.parse(new Date(this.dayEvents.date)), i18n[this.locale].fullFormat)
+          return `${tempDate} ${i18n[this.locale].notHaveEvents}`
+        }
       // } else {
       //   return i18n[this.locale].dayEventsTitle
       // }
